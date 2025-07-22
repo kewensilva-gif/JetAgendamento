9fff3c59875738484ca85bcdfb80e7341db863e1
KEWEN SILVA <kewensilva58@gmail.com>
2025-07-22 20:56:53 -0300

    [Fix: ajuste na tabela task, adiciona atributo title]

--- prisma/migrations/20250722234208_init/migration.sql
+++ prisma/migrations/20250722234208_init/migration.sql
@@ -9,6 +9,7 @@ CREATE TABLE "User" (
 -- CreateTable
 CREATE TABLE "Task" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "dueDate" DATETIME NOT NULL,
     "status" TEXT NOT NULL DEFAULT 'PENDENTE',
